2e3825a91d413c1ae54d1dcea66dfa702781d434
hanif-github-1 <hanif.github.1@gmail.com>
2023-10-08 15:02:50 +0330

    some minor changes

--- app/utils.py
+++ app/utils.py
@@ -2,14 +2,23 @@ from psycopg.rows import dict_row
 import psycopg
 import time
 
-
+counter = 0
 while True:
+    counter += 1
     try:
         conn = psycopg.connect(
             host="localhost", port="5433", dbname="fastapi", user="postgres", password="1234", row_factory=dict_row
         )
         cursor = conn.cursor()
-        break
     except Exception as error:
-        pass
+        print("OOPS :(")
+        if counter == 10:
+            del counter
+            conn = psycopg.connect(
+                host="localhost", port="5433", dbname="fastapi", user="postgres", password="1234", row_factory=dict_row
+            )
+            break
+    else:
+        del counter
+        break
     time.sleep(2)
